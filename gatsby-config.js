23009503551689258d58dab9b888c23c9a012462
Daniel Graham <dfgraham@gmail.com>
2019-10-14 18:29:17 -0400

    feat: add country club icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,13 +54,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `The Country Club Blog`,
+        short_name: `CountryCloub`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/country-club.png`,
       },
     },
     `gatsby-plugin-offline`,
